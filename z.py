e107d7ff5ccae783045957b0764f37645246a6fa
nikita133724 <33579082+nikita133724@users.noreply.github.com>
2026-01-17 21:39:11 +0600

    Update z.py

--- z.py
+++ z.py
@@ -1,12 +1,14 @@
-from playwright.sync_api import sync_playwright
 
-url = "https://csgoyz.run/raffles"  # сюда вставь нужный сайт
+from playwright.sync_api import sync_playwright
+import os
+
+url = os.environ.get("TARGET_URL", "https://csgoyz.run/raffles")  # URL берём из переменной окружения
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)  # без окна
+    browser = p.chromium.launch(headless=True)
     page = browser.new_page()
-    page.goto(url, wait_until="networkidle")  # ждём выполнения JS
-    html = page.content()  # финальный HTML
+    page.goto(url, wait_until="networkidle")
+    html = page.content()
     browser.close()
 
-print(html)  # выводим HTML в логи
+print("[HTML]:", html)  # вывод в логи Render
