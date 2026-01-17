c979ed713ab2607e012062a1d8cda41cc1f4880b
nikita133724 <33579082+nikita133724@users.noreply.github.com>
2026-01-17 22:55:40 +0600

    Update z.py

--- z.py
+++ z.py
@@ -2,13 +2,14 @@
 from playwright.sync_api import sync_playwright
 import os
 
-url = os.environ.get("TARGET_URL", "https://csgoyz.run/raffles")  # URL берём из переменной окружения
+url = os.environ.get("TARGET_URL", "https://csgoyz.run/raffles")
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
     page.goto(url, wait_until="networkidle")
+    page.wait_for_timeout(5000)  # ждём 5 секунд для выполнения JS
     html = page.content()
     browser.close()
 
-print("[HTML]:", html)  # вывод в логи Render
+print("[HTML]:", html)
