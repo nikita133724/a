1157ae15a4f0f1b6c7bb4bc5bf64c4ed562b801e
nikita133724 <33579082+nikita133724@users.noreply.github.com>
2026-01-17 21:15:26 +0600

    Update z.py

--- z.py
+++ z.py
@@ -1,9 +1,11 @@
-import requests
+from requests_html import HTMLSession
 from pathlib import Path
 
 URL = "https://csgoyz.run/raffles"
 OUT_FILE = Path("raffles_dom.html")
 
-r = requests.get(URL)
-OUT_FILE.write_text(r.text, encoding="utf-8")
+session = HTMLSession()
+r = session.get(URL)
+r.html.render(sleep=3, keep_page=False)  # JS выполнится
+OUT_FILE.write_text(r.html.html, encoding="utf-8")
 print(f"[OK] HTML saved to {OUT_FILE.resolve()}")
